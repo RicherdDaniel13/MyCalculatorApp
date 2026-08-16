709a667f7474d69c652b9f8b10b2ca688881326f
RicherdDaniel13 <144140906+RicherdDaniel13@users.noreply.github.com>
2026-08-16 20:30:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Node.js Calculator API is running!");
+  res.send("Node.js Calculator API is running....!");
 });
 
 app.get("/add", (req, res) => {
